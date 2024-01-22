be8cf61da18375303d769a60b8d87e1f87344b3f
lakota <lakotajcamp@gmail.com>
2024-01-22 12:02:36 -0500

    mostly done with models, added some of the front end

--- Backend/model.py
+++ Backend/model.py
@@ -1,24 +1,33 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, create_engine
+from sqlalchemy import ForeignKey, Column, Integer, String, create_engine, MetaData
 from sqlalchemy.orm import Session, declarative_base, relationship, validates
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
+from flask_sqlalchemy import SQLAlchemy
 
 from config import db, bcrypt
 
 Base = declarative_base()
 
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+
+db = SQLAlchemy(metadata=metadata)
 
 class Trainer(db.Model, SerializerMixin):
     __tablename__ = 'trainers'
-# id that is an integer type and a primary key.
     id = db.Column(db.Integer, primary_key = True)
-# trainer that is a String type.
-    trainer = db.Column(db.String, unique = True, nullable = False)
-# _password_hash that is a String type.
+    username = db.Column(db.String, unique = True, nullable = False)
     _password_hash = db.Column(db.String)
-
+    #relationship column:
     poke_teams = db.relationship('PokeTeam', back_populates = "trainer")
-    
+    #serialzier:
     serialize_rules = ('-poke_teams.trainer',)
 
     @hybrid_property
@@ -26,6 +35,7 @@ class Trainer(db.Model, SerializerMixin):
         raise AttributeError("Can't access this")
         # return self._password_hash
     
+    #This is boiler plate code:
     @password_hash.setter
     def password_hash(self, password):
         hashed_password = bcrypt.generate_password_hash(password.encode('utf-8'))
@@ -34,6 +44,7 @@ class Trainer(db.Model, SerializerMixin):
     def authenticate(self,password):
         return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
     
+    #Validation: Must have Trainer
     @validates('trainer')
     def validate_trainer(self,key,value):
         if value:
@@ -41,24 +52,28 @@ class Trainer(db.Model, SerializerMixin):
         else:
             raise ValueError("Not valid trainer")
 
-
+#This is the table that has full crud.
 class Team(Base,SerializerMixin):
     __tablename__= "teams"
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String)
+    #relationship columns:
     poke_teams = db.relationship("PokeTeam", back_populates="team")
-    serialize_rules=('-poke_teams.team',)
     trainer_id = db.Column(db.Integer, db.ForeignKey("trainers.id"))
     trainer = db.relationship("Trainer", back_populates="team")
+    #serializer:
+    serialize_rules=('-poke_teams.team','-trainer.team')
 
     def __repr__(self):
-        return repr(f"{self.name}")
+        return repr(f"{self.name}, {self.trainer}")
 
 class Pokemon(Base):
     __tablename__= "pokemons"
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String)
+    #relationship column:
     poke_teams = db.relationship("PokeTeam", back_populates="pokemon")
+    #serializer:
     serialize_rules=('-poke_teams.pokemon',)
 
     def __repr__(self):
@@ -68,15 +83,16 @@ class Pokemon(Base):
 class PokeTeam(Base):
     __tablename__ = "poke_teams"
     id = db.Column(db.Integer, primary_key = True)
+    #Relationship columns:
     team_id = db.Column(db.Integer, ForeignKey("teams.id"))
     team = db.relationship("Team", back_populates="poke_teams")
     pokemon_id = db.Column(db.Integer, ForeignKey("pokemons.id"))
     pokemon = db.relationship("Pokemon", back_populates="poke_teams")
-
-    serialize_rules = ('-trainer.poke_teams','')
+    #Serializer:
+    serialize_rules = ('-team.poke_teams','-pokemon.poke_teams')
 
     def __repr__(self):
-        return repr(f"{self.team}, {self.pokemon}, {self.trainer}")
+        return repr(f"{self.team}, {self.pokemon}")
 
 
-#engine = create_engine("sqlite:///darktide_builder.db")
+#engine = create_engine("sqlite:///pokedex.db")
---
+++ Backend/seed.py
@@ -0,0 +1,25 @@
+import requests
+from app import app 
+from model import db, Team, Trainer, Pokemon, PokeTeam
+from random import randint, choice as rc
+from faker import Faker
+
+def seed_pokemon_data():
+    api_url = 'https://pokeapi.co/api/v2/pokemon/'
+    response = requests.get(api_url)
+
+    if response.status_code: 
+        pokemon_data = response.json()
+        print(pokemon_data)
+        for pokemon in pokemon_data['results']:
+            name = pokemon['name']
+            new_pokemon = Pokemon(**Pokemon(name=name).to_dict())
+            db.session.add(new_pokemon)
+            db.session.commit()
+
+            print('Pokemon data seeded successfully.')
+    else:
+        print('Failed to fetch Pokemon data')
+
+if __name__ == '__main__':
+    seed_pokemon_data()
