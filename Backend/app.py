3aa3305949bce895ca7962f19bfb89ad25c42b05
lakota <lakotajcamp@gmail.com>
2024-01-24 06:22:48 -0500

    got the delete team button fully functional on main page

--- Backend/app.py
+++ Backend/app.py
@@ -1,5 +1,4 @@
 from flask import request, session
-#from flask_restful import Resource, 
 from sqlalchemy.exc import IntegrityError
 from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
@@ -10,16 +9,6 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get(
     "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
-
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.json.compact = False
-
-# migrate = Migrate(app, db)
-
-# db.init_app(app)
 
 
 @app.route('/')
@@ -72,7 +61,7 @@ def login():
             return {"Error": "Not valid trainer name or password"}, 401
         
 
-@app.route('/lougout', methods=['DELETE'])
+@app.route('/logout', methods=['DELETE'])
 def logout():
     if request.method == 'DELETE':
         session['trainer_id'] = None
@@ -161,10 +150,10 @@ def save_team():
                 # name6 = data['name6']
 
 """
+api.add_resource(Logout, '/logout', endpoint='logout')
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 api.add_resource(Login, '/login', endpoint='login')
-api.add_resource(Logout, '/logout', endpoint='logout')
 """
 
 if __name__ == '__main__':
--- Frontend/src/pages/MainPage.jsx
+++ Frontend/src/pages/MainPage.jsx
@@ -2,37 +2,50 @@ import React, { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import { Link } from "react-router-dom";
 import { Button } from "../styles";
-//import CreateTeam from "/src/pages/CreateTeam"
+
+
 
 function TeamList() {
   const [teams, setTeams] = useState([]);
-
+  
   useEffect(() => {
     fetch("/api/teams")
-      .then((r) => r.json())
-      .then(setTeams);
+    .then((r) => r.json())
+    .then(setTeams);
   }, []);
-
+  
+  const deleteTeam = (teamId) => {
+    fetch(`/api/teams/${teamId}`, {
+      method: "DELETE",
+    })
+    .then(() => {
+      setTeams((deletedTeams) => deletedTeams.filter((team) => team.id !== teamId));
+    })
+  };
+  
   return (
+    <div className="Wrapper">
+      <h1 className="Logo">PokeDecks</h1>
     <div>
       {teams.length > 0 ? (
-            <ul>
-              {teams.map(team => (
-                <li>
-                  {team.name}
-                </li>
-              ))}
-            </ul>
+        <ul>
+          {teams.map((team) => (
+            <li key={team.id}>
+              {team.name}
+              <button onClick={() => deleteTeam(team.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
       ) : (
         <>
           <h2>No Teams Found</h2>
         </>
       )}
-          <Button as={Link} to="/create-team">
-            Build a New Team
-          </Button>
+      <Button as={Link} to="/create-team">
+        Build a New Team
+      </Button>
+    </div>
     </div>
   );
 }
-// still needs a logout button
 export default TeamList;
